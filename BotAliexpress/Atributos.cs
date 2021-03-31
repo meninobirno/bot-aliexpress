f152828ce8a3e9cfea3cee635f741bff50905593
bini <vinicioschubert@hotmail.com>
2021-03-31 11:28:46 -0300

    ORIENTAÇÃO OBJETO BICHO

---
+++ BotAliexpress/Atributos.cs
@@ -0,0 +1,11 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium.Chrome;
+
+namespace BotAliexpress
+{
+    public class Atributos
+    {
+        protected static readonly ChromeDriver chromeDriver = new ChromeDriver();
+        protected static readonly HtmlDocument html = new HtmlDocument();
+    }
+}
---
+++ BotAliexpress/Bot.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+
+namespace BotAliexpress
+{
+    public class Bot : Atributos
+    {
+        #region CarregaURL
+        protected static void CarregaUrl(string url)
+        {
+            chromeDriver.Navigate().GoToUrl(url);
+            html.LoadHtml(chromeDriver.PageSource);
+        }
+        #endregion
+
+        #region ValidaUrl
+        protected static bool ValidaUrl(string url)
+        {
+            while(chromeDriver.Url != url)
+            {
+                continue;
+            }
+            return true;
+        }
+        #endregion
+
+        #region EfetuaCompra
+        protected static void EfetuaCompra(string valorInicial, DateTime horarioInicio, bool valid)
+        {
+            while (!valid)
+            {
+                while (DateTime.Parse(DateTime.Now.ToString("HH:mm:ss")) >= horarioInicio)
+                {
+                    try
+                    {
+                        string validacaoPagamento = html.DocumentNode.SelectSingleNode("//div[@class='pay-brief-info']").InnerText;
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        SelecionaPagamento();
+                    }
+
+                    //Inserir o cupom e finalizar a compra
+                    while (true)
+                    {
+                        //Código do cupom de desconto
+                        chromeDriver.FindElementByXPath("//div[@class='next-form-item-control']//input").SendKeys("ALIBR99");
+                        Thread.Sleep(500);
+                        chromeDriver.FindElementByXPath("//div[@class='next-form-item-control']//button").Click();
+                        Thread.Sleep(500);
+                        chromeDriver.FindElementByXPath("//div[@id='root']").Click();
+                        Thread.Sleep(500);
+
+                        html.LoadHtml(chromeDriver.PageSource);
+                        string valorFinal = html.DocumentNode.SelectSingleNode("//div[@class='total-price']").InnerText;
+
+                        //Verifica se o valor foi alterado
+                        if (valorInicial != valorFinal)
+                        {
+                            break;
+                        }
+                    }
+                    chromeDriver.FindElementByXPath("//div[@class='order-btn-holder']//button").Click();
+                    valid = true;
+                    break;
+                }
+            }
+        }
+        #endregion
+
+        #region SelecionaPagamento
+        private static void SelecionaPagamento()
+        {
+            chromeDriver.FindElementByXPath("//button[@class='next-btn next-medium next-btn-primary next-btn-text selected-payment-btn']").Click();
+            Thread.Sleep(500);
+            chromeDriver.FindElementByXPath("//div[@ae_object_value = 'select_payoption=BOLETO']").Click();
+            Thread.Sleep(500);
+            chromeDriver.FindElementByXPath("//div[@class='save']//button").Click();
+            Thread.Sleep(500);
+        }
+        #endregion
+    }
+}
